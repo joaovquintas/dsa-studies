ad8f82b0542badd105eb99efa2c0b9b5ea568056
joaovquintas <jvqs99@outlook.com>
2022-12-10 19:44:06 -0300

    linear search and binary search(iterative and recursive)

--- 03- Arrays/Array ADT/display.c
+++ 03- Arrays/Array ADT/display.c
@@ -32,9 +32,87 @@ void Insert(struct Array *arr, int index, int x){
         arr->A[index] = x;
         arr->length++;
     }
-    
+}
 
 
+
+int Delete(struct Array *arr, int index){
+
+    int x=0;
+
+    if(index >= 0 && index <= arr->length-1){
+        x = arr->A[index];
+
+        for(int i = index; i < arr->length-1; i++){
+            arr->A[i] = arr->A[i+1];
+        }
+        arr->length--;
+        return x;
+
+    }                                 
+    return 0;
+}
+
+void swap(int *x, int *y){
+
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+
+
+int LinearSearch(struct Array *arr, int key ){ //no pointer need cuz the search will look for a value direclty. 
+
+    for(int i = 0; i < arr->length; i++){
+        
+        if(key == arr->A[i]){
+            //transposition:
+            //swap(&arr->A[i], &arr->A[i-1]);
+            //return i;
+
+            //move to head: 
+            swap(&arr->A[i], &arr->A[0]);
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int BinarySearch(struct Array arr, int key){
+
+    int lower, higher, mid;
+    lower = 0;
+    higher = arr.length - 1;
+
+    while(lower <= higher){
+
+        mid = (lower+higher)/2;
+        if(key == arr.A[mid])
+            return mid;
+        else if(key < arr.A[mid])
+            higher = mid-1;
+        else
+            lower = mid+1;
+    }
+    return -1;
+}
+
+int RecursiveBinarySearch(int a[], int lower, int higher, int key){
+    int mid;
+
+    if(lower <= higher){
+        mid = (lower+higher)/2;
+        
+        if(key == a[mid])
+            return mid;
+        else if(key < a[mid])
+            return RecursiveBinarySearch(a, lower, mid-1, key);
+        else   
+            return RecursiveBinarySearch(a, mid+1, higher, key);
+    }
+    return -1;
 }
 
 int main(){
@@ -54,13 +132,17 @@ int main(){
         scanf("%d", &arr.A[i]);
     arr.length = n;
 
-    Insert(&arr, 0, 110);
+    //Insert(&arr, 0, 110);
+
     //Append(&arr, 10);
+
+    //printf("\n %d \n", Delete(&arr, 0));
+
+    //printf("\n %d \n", LinearSearch(&arr, 4));
+
+    //printf("\n %d \n", RecursiveBinarySearch(arr.A,0,arr.length,6));
+
     Display(arr);
-
-
-
-
 
 
     return 0;
